b9263122fd2b349a4609b8af8d6da3bcae3fa196
kimie-shen <ks44@princeton.edu>
2024-01-04 18:33:14 -0500

    cube clean up

---
+++ cube/cube_energy_level_plotter.m
@@ -0,0 +1,32 @@
+
+% Plot energy levels
+figure(1)
+for i = 1:length(energy_levels(:,1))
+    x1 = [0.5, 1.5];
+    x2 = [1.5, 2.5];
+    x3 = [2.5, 3.5];
+    x4 = [3.5, 4.5];
+    y = [-energy_levels(i, 1), -energy_levels(i, 1)];
+
+    if (energy_levels(i, 2) == 1)
+        plot(x1, y,'-r')
+        hold on
+    end
+
+    if (energy_levels(i, 2) == 2)
+        plot(x2, y, 'g')
+        hold on
+    end
+
+    if (energy_levels(i, 2) == 3)
+        plot(x3, y, 'b')
+        hold on
+    end
+
+    if (energy_levels(i, 2) > 3)
+        plot (x4, y, 'm')
+    end
+
+end
+
+hold off
--- cube/spacings.m
+++
@@ -1,4 +0,0 @@
-spacings = zeros(6 * N^2 - 1);
-for i=1:(6*N^2 - 1)
-    spacings(i) = (abs(e(i)) - abs(e(i+1)));
-end
