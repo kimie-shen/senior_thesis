4036ec752ddcc4ba7be34c901a239a5fcd5032cb
kimie-shen <ks44@princeton.edu>
2024-01-04 18:38:45 -0500

    clean up

--- square_prism/test3.m
+++
@@ -1,7 +0,0 @@
-N_nums = [5, 7, 11, 13, 17, 19, 23, 29, 31, 37];
-plot(N_nums, solvable_prop, '-o')
-title('Proportion of states which are solvable')
-xlabel('N')
-yline(1/4, '--', '1/4')
-ylim([0.245, 0.255])
-ylabel('Proportion')
